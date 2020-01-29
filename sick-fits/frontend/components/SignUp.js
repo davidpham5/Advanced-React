a425e7e283e6c8de885feaade4e81896643243b9
David Pham <davidpham5@gmail.com>
2020-01-28 22:27:10 -0500

    markup for sign up.

--- sick-fits/frontend/components/SignUp.js
+++ sick-fits/frontend/components/SignUp.js
@@ -11,12 +11,37 @@ const ContainerStyles = styled.div`
   margin: 0 auto;
 `;
 class SignUp extends Component{
+  state = {
+    name: '',
+    password: '',
+    email: ''
+  }
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
   render () {
     return (
       <ContainerStyles>
         <Form>
           <fieldset>
             <h2>Sign up</h2>
+            <label htmlFor="name" >
+              Name
+              <input type="name" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange}/>
+            </label>
+            <label htmlFor="email">
+              Email
+              <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
+            </label>
+            <label htmlFor="password">
+              Password
+              <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}/>
+            </label>
+
+            <input type="submit" value="Submit"/>
           </fieldset>
         </Form>
       </ContainerStyles>
